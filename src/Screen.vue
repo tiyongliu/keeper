c9f8253bc418d00c1c21b0f1c6ba14ee02315038
刘体勇 <422720735@qq.com>
2022-03-14 21:58:26 +0800

    初始化

---
+++ src/Screen.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>left</div>
+    <div>content</div>
+    <div>right</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Screen"
+    }
+</script>
+
+<style scoped>
+
+</style>
