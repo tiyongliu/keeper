f97da2435658129deea0e94d7f451bd18df3c8c9
刘体勇 <422720735@qq.com>
2022-03-20 22:31:18 +0800

    拖拽窗口大小。

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -4,8 +4,10 @@
 import type { App } from 'vue';
 import { setupPermissionDirective } from './permission';
 import { setupLoadingDirective } from './loading';
+import { setupSplitterDrag } from './splitterDrag';
 
 export function setupGlobDirectives(app: App) {
   setupPermissionDirective(app);
   setupLoadingDirective(app);
+  setupSplitterDrag(app);
 }
