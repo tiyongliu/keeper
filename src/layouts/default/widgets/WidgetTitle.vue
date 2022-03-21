c960abe4b944461742c59d07d4cb480eda5a25f4
刘体勇 <422720735@qq.com>
2022-03-21 18:09:08 +0800

    编写基础组件

---
+++ src/layouts/default/widgets/WidgetTitle.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <slot />
+    </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed, unref, ref, onMounted } from 'vue';
+  export default defineComponent({
+    name: "WidgetTitle",
+    components: {
+
+    },
+    setup() {
+      const hidden = ref(false)
+
+      return {
+        hidden
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  div {
+    padding: 5px;
+    font-weight: bold;
+    text-transform: uppercase;
+    background-color: var(--theme-bg-1);
+    border: 2px solid var(--theme-border);
+  }
+  div:hover {
+    background-color: var(--theme-bg-2);
+  }
+</style>
+
