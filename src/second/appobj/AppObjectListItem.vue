b0aad6e89d9c10fca26bf360550006c5ee17f200
刘体勇 <422720735@qq.com>
2022-03-27 00:21:45 +0800

    继续编辑

---
+++ src/second/appobj/AppObjectListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="subitems"></div>
+</template>
+
+<script lang="ts">
+  import {computed, defineComponent, PropType, unref} from 'vue';
+  export default defineComponent({
+    name: "AppObjectListItem",
+    props: {
+      data: {
+        type: Array as unknown as PropType<[]>,
+        default: [],
+      }
+    },
+    components: {},
+    setup(props) {
+      // const dynamicList = computed(() => unref(props.list))
+
+      return {
+
+      }
+    }
+  })
+</script>
+
+<style lang="less">
+  .subitems {
+    margin-left: 28px;
+  }
+</style>
---
+++ src/second/widgets/ConnectionList.vue
@@ -0,0 +1,54 @@
+<template>
+  <SearchBoxWrapper>
+    <SearchInput placeholder="Search connection or database" v-model:value="filter"/>
+    <CloseSearchButton :filter="filter" @close="filter = ''"/>
+    <InlineButton title="Add new connection">
+      <plus-outlined/>
+    </InlineButton>
+
+    <InlineButton title="Add new connection">
+      <redo-outlined/>
+    </InlineButton>
+  </SearchBoxWrapper>
+  <WidgetsInnerContainer>
+    <AppObjectList />
+  </WidgetsInnerContainer>
+</template>
+
+<script lang="ts">
+  import {defineComponent, ref} from 'vue';
+  import SearchBoxWrapper from './SearchBoxWrapper.vue'
+  import WidgetsInnerContainer from './WidgetsInnerContainer.vue'
+  import SearchInput from '/@/second/elements/SearchInput.vue'
+  import InlineButton from '/@/second/buttons/InlineButton.vue'
+  import CloseSearchButton from '/@/second/buttons/CloseSearchButton.vue'
+  import AppObjectList from '/@/second/appobj/AppObjectList.vue'
+  import {PlusOutlined, RedoOutlined} from '@ant-design/icons-vue'
+
+  export default defineComponent({
+    name: "ConnectionList",
+    components: {
+      SearchBoxWrapper,
+      WidgetsInnerContainer,
+      CloseSearchButton,
+      SearchInput,
+      InlineButton,
+      RedoOutlined,
+      PlusOutlined,
+      AppObjectList,
+    },
+    setup() {
+      const hidden = ref(false)
+      const filter = ref('')
+
+      return {
+        hidden,
+        filter
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
