f0bda08886f31165b86cd4f4b360001d0e23fa33
刘体勇 <422720735@qq.com>
2022-03-30 20:34:37 +0800

    comp

--- src/second/appobj/AppObjectList.tsx
+++ src/second/appobj/AppObjectList.tsx
@@ -1,7 +1,9 @@
 import {computed, defineComponent, onMounted, PropType, unref} from 'vue'
-import getLocalStorage from '/@/second/utility/getConnectionLabel'
-import {filterName} from '/@/packages/tools/src/filterName'
+import {compact} from 'lodash'
 import AppObjectListItem from './AppObjectListItem.vue'
+
+type fn = (data: {_id: string, singleDatabase: boolean}) => boolean
+
 export default defineComponent({
   name: "DatabaseWidget",
   components: {AppObjectListItem},
@@ -11,33 +13,66 @@ export default defineComponent({
     },
     groupFunc: {
       type: String as PropType<string>,
+    },
+    expandOnClick: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    isExpandable: {
+      type: Function as PropType<fn>
+    },
+    filter: {
+      type: String as PropType<string>,
     }
   },
   setup(props) {
     // const dynamicList = computed(() => unref(props.list))
-    const {list, groupFunc} = props
+    const {list, groupFunc, filter, isExpandable} = props
 
     console.log(list, '18')
     const filtered = computed(() => {
-      !unref(groupFunc) ? list.filter(data => {
-
+      !unref(groupFunc) ? (list!).filter(data => {
+        const matcher = createMatcher && createMatcher(data);
+        if (matcher && !matcher(filter)) return false;
+        return true;
       }) : null
     })
 
+    const childrenMatched = computed(() => {
+      !unref(groupFunc) ? (list!).filter(data => {
+        const matcher = createChildMatcher && createChildMatcher(data)
+        if (matcher && !matcher(filter)) return false;
+        return true
+      }) : null
+    })
+
+    const listGrouped = computed(() => {
+      unref(groupFunc) ? compact(
+        ((list!) || []).map(data => {
+          const matcher = createMatcher && createMatcher(data);
+          const isMatched = matcher && !matcher(filter) ? false : true;
+        })
+      ) : null
+    })
+
     onMounted(() => {
-      setTimeout(() => {
-        console.log(props.list)
-      }, 2000)
     })
 
     return () => (
       (list!).map(data => {
-        return <AppObjectListItem {...data}/>
+        return <AppObjectListItem
+          isHidden={!(filtered as unknown as []).includes(data)}
+          data={data}
+          isExpandable={isExpandable}
+        />
       })
 
     )
   }
 })
+
+import getLocalStorage from '/@/second/utility/getConnectionLabel'
+import {filterName} from '/@/packages/tools/src/filterName'
 
 export const extractKey = data => data._id;
 export const createMatcher = props => filter => {
--- src/second/appobj/ConnectionAppObject.tsx
+++ src/second/appobj/ConnectionAppObject.tsx
@@ -1,5 +1,8 @@
 import {computed, defineComponent, onMounted, PropType, unref} from 'vue'
+import {getLocalStorage} from '/@/second/utility/storageCache'
 
 export default defineComponent({
 
 })
+
+export const extractKey = data => data._id;
