3db69209f4d812d96686d3173c3e67faba6f5aa5
liutiyong <422720735@qq.com>
2022-05-26 11:43:41 +0800

    编写测试方法

--- app/src/plugins/plugin-mondb/drivers.go
+++ app/src/plugins/plugin-mondb/drivers.go
@@ -1,14 +1,11 @@
 package plugin_mondb
 
 import (
-	"bytes"
 	"context"
-	"dbbox/app/src/modules"
 	"dbbox/app/src/pkg/standard"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoDBDrivers struct {
@@ -19,61 +16,13 @@ func NewMongoDB() standard.SqlStandard {
 	return &MongoDBDrivers{}
 }
 
-func (mongdb *MongoDBDrivers) GetPoolInfo() interface{} {
-	return mongdb.DB
+func (mg *MongoDBDrivers) GetPoolInfo() interface{} {
+	return mg.DB
 }
 
-func NewSimpleMongoDBPool(setting *modules.SimpleSettingMongoDB) (standard.SqlStandard, error) {
-	if setting == nil {
-		return nil, fmt.Errorf("setting is nil")
-	}
-	if setting.Username == "" {
-		//return nil, fmt.Errorf("lack of setting.Username")
-	}
-	if setting.Password == "" {
-		//return nil, fmt.Errorf("lack of setting.Password")
-	}
-	if setting.Host == "" {
-		return nil, fmt.Errorf("lack of setting.Host")
-	}
-	if setting.Port == "" {
-		return nil, fmt.Errorf("lack of setting.Port")
-	}
-
-	// mongoUrl := "mongodb://" + user + ":" + password + "@" + url + "/" + dbname
-	//clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	var buf bytes.Buffer
-	buf.WriteString("mongodb://")
-	if setting.Username != "" {
-		buf.WriteString(setting.Username)
-		buf.WriteString(":")
-	}
-
-	if setting.Password != "" {
-		buf.WriteString(setting.Password)
-		buf.WriteString("@")
-	}
-
-	buf.WriteString(setting.Host)
-	buf.WriteString(":")
-	buf.WriteString(setting.Port)
-
-	db, err := mongo.Connect(context.Background(), options.Client().ApplyURI(buf.String()))
-	if err != nil {
-		return nil, err
-	}
-
-	//err = db.Ping(context.Background(), nil)
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	return &MongoDBDrivers{db}, nil
-}
-
-func (mongdb *MongoDBDrivers) GetVersion() (interface{}, error) {
-	defer mongdb.DB.Disconnect(context.Background())
-	db := mongdb.DB.Database("local")
+func (mg *MongoDBDrivers) GetVersion() (interface{}, error) {
+	//defer mg.DB.Disconnect(context.Background())
+	db := mg.DB.Database("local")
 
 	buildInfoCmd := bson.D{bson.E{Key: "buildInfo", Value: 1}}
 	var buildInfoDoc bson.M
@@ -86,16 +35,26 @@ func (mongdb *MongoDBDrivers) GetVersion() (interface{}, error) {
 	}, nil
 }
 
-func (mongdb *MongoDBDrivers) ListDatabases() (interface{}, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		return nil, err
-	}
-	defer client.Disconnect(context.Background())
+type List struct {
+	Databases []*struct {
+		Name       string `bson:"name" json:"name"`
+		SizeOnDisk int    `bson:"sizeOnDisk" json:"sizeOnDisk"`
+		Empty      bool   `bson:"empty" json:"empty"`
+	} `bson:"databases" json:"databases"`
+	TotalSize int `bson:"totalSize" json:"totalSize"`
+	Ok        int `bson:"ok" json:"ok"`
+}
 
-	_, err = client.ListDatabaseNames(context.Background(), bson.M{})
-	//databases, err := client.ListDatabaseNames(context.Background(), bson.M{})
+func (mg *MongoDBDrivers) ListDatabases() (interface{}, error) {
+	buildInfoCmd := bson.D{bson.E{Key: "listDatabases", Value: 1}}
+	var buildInfoDoc List
 
-	return nil, err
+	//databases, err := mg.DB.ListDatabaseNames(context.Background(), bson.M{})
+	db := mg.DB.Database("admin")
+	err := db.RunCommand(context.TODO(), buildInfoCmd).Decode(&buildInfoDoc)
+	return buildInfoDoc, err
+}
+
+func (mg *MongoDBDrivers) Close() error {
+	return mg.DB.Disconnect(context.Background())
 }
---
+++ app/src/plugins/plugin-mondb/pool.go
@@ -0,0 +1,47 @@
+package plugin_mondb
+
+import (
+	"bytes"
+	"context"
+	"dbbox/app/src/modules"
+	"dbbox/app/src/pkg/standard"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewSimpleMongoDBPool(setting *modules.SimpleSettingMongoDB) (standard.SqlStandard, error) {
+	if setting == nil {
+		return nil, fmt.Errorf("setting is nil")
+	}
+	if setting.Port == "" {
+		return nil, fmt.Errorf("lack of setting.Port")
+	}
+	var buf bytes.Buffer
+	buf.WriteString("mongodb://")
+	if setting.Username != "" {
+		buf.WriteString(setting.Username)
+		buf.WriteString(":")
+	}
+
+	if setting.Password != "" {
+		buf.WriteString(setting.Password)
+		buf.WriteString("@")
+	}
+
+	buf.WriteString(setting.Host)
+	buf.WriteString(":")
+	buf.WriteString(setting.Port)
+
+	db, err := mongo.Connect(context.Background(), options.Client().ApplyURI(buf.String()))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MongoDBDrivers{db}, nil
+}
