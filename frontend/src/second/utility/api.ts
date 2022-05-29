9b3378cdb3ae5b0d001f9f572ec90e38f77d8002
liuliutiyong <422720735>
2022-05-30 01:08:53 +0800

    数据库连接编写

--- frontend/src/second/utility/api.ts
+++ frontend/src/second/utility/api.ts
@@ -1,24 +1,15 @@
 import {useGlobSetting} from '/@/hooks/setting';
 import {defHttp} from '/@/utils/http/axios';
 
-const {drivers, environment} = useGlobSetting()
+const {environment} = useGlobSetting()
 
-export async function apiCall<T>(relativePath: string, params?: T): Promise<T> {
+export async function apiCall<T>(relativePath: string, params?: T): Promise<T | void> {
   //读取环境变量
   console.log('>>> API CALL', relativePath, params)
 
   if (environment === 'web') {
-    const resp = await defHttp.post({url: relativePath, params})
-    return resp
-  } else {
-   try {
-     const resp = await window['go']['backend']['MMMM']['GetVersion'](params)
-     console.log(resp)
-     return resp
-   } catch (e) {
-     console.log(e)
-     return e
-   }
+    return await defHttp.post({url: relativePath, params})
   }
-  return
+
+  // const resp = await window['go']['backend']['MMMM']['GetVersion'](params)
 }
