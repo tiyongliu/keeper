f42945d7919270689f5975207f18c5a1f7a66554
liuliutiyong <422720735>
2022-05-31 23:11:06 +0800

    弹窗

--- frontend/src/api/connection.ts
+++ frontend/src/api/connection.ts
@@ -5,6 +5,5 @@ import {apiCall} from '/@/second/utility/api'
 * @param {Object} params 连接信息
 */
 export async function handleDriverTestApi(params) {
-  const resp = await apiCall('Connections.Test', params)
-  return resp
+  return await apiCall('Connections.Test', params)
 }
--- frontend/wailsjs/go/main/App.js
+++ frontend/wailsjs/go/main/App.js
@@ -5,3 +5,7 @@
 export function Greet(arg1) {
   return window['go']['main']['App']['Greet'](arg1);
 }
+
+export function OpenDirectoryDialog(arg1) {
+  window['go']['main']['App']['OpenDirectoryDialog'](arg1);
+}
