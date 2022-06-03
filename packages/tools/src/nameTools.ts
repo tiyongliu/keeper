ff92a401e585b70ac5da33d5d1e09a9e7e374fe3
liuliutiyong <422720735>
2022-06-04 01:22:12 +0800

    dialog

--- packages/tools/src/nameTools.ts
+++ packages/tools/src/nameTools.ts
@@ -1,4 +1,4 @@
-import {TableInfo, ColumnInfo} from 'dbbox-types'
+import {TableInfo, ColumnInfo} from 'keeper-types'
 
 export function findForeignKeyForColumn(table: TableInfo, column: ColumnInfo) {
   return (table.foreignKeys || []).find(fk => fk.columns.find(col => col.columnName == column.columnName));
--- tools/src/nameTools.ts
+++
@@ -1,5 +0,0 @@
-import {TableInfo, ColumnInfo} from 'dbbox-types'
-
-export function findForeignKeyForColumn(table: TableInfo, column: ColumnInfo) {
-  return (table.foreignKeys || []).find(fk => fk.columns.find(col => col.columnName == column.columnName));
-}
