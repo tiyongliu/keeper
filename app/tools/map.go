397d31cf372726039995c927f152a56bd7d0aef6
liutiyong <422720735@qq.com>
2022-06-06 19:54:06 +0800

    数据库连接

--- app/tools/map.go
+++ app/tools/map.go
@@ -9,3 +9,12 @@ func MapBoundString(m map[string]interface{}) map[string]string {
 	}
 	return ret
 }
+
+func LooseMapValue(m map[string]string) map[string]interface{} {
+	nm := make(map[string]interface{})
+	for k, v := range m {
+		nm[k] = v
+	}
+
+	return nm
+}
