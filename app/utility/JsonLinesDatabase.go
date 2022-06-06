254c5ecd47f088afb07897b9003aa7daba36d8b7
liutiyong <422720735@qq.com>
2022-06-06 18:31:47 +0800

    写文件

--- app/utility/JsonLinesDatabase.go
+++ app/utility/JsonLinesDatabase.go
@@ -1,9 +1,12 @@
 package utility
 
 import (
+	"bufio"
 	"fmt"
 	uuid "github.com/satori/go.uuid"
+	"io/ioutil"
 	"keeper/app/tools"
+	"os"
 	"sync"
 )
 
@@ -78,4 +81,45 @@ func (j *JsonLinesDatabase) save() {
 	}
 	//写文件
 
+}
+
+func WriteToFile(fileName string, content string) error {
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("file create failed. err: " + err.Error())
+	} else {
+		// offset
+		//os.Truncate(filename, 0) //clear
+		n, _ := f.Seek(0, os.SEEK_END)
+		_, err = f.WriteAt([]byte(content), n)
+		fmt.Println("write succeed!")
+		defer f.Close()
+	}
+	return err
+}
+
+//将map[string]string写入文件
+func WriteMaptoFile(m map[string]string, filePath string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("create map file error: %v\n", err)
+		return err
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	for k, v := range m {
+		lineStr := fmt.Sprintf("%s^%s", k, v)
+		fmt.Fprintln(w, lineStr)
+	}
+	return w.Flush()
+}
+
+//这种方式每次都会覆盖 test.txt内容，如果test.txt文件不存在会创建。
+func writeMap() {
+	content := []byte("测试1\n测试2\n")
+	err := ioutil.WriteFile("test.txt", content, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
