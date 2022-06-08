e3483b9f1a6f03917b80ce50551d8db2559db104
tiyongliu <tiyongliu@163.com>
2022-06-09 00:36:26 +0800

    文件保存还需要修改

--- app/tools/filePool.go
+++ app/tools/filePool.go
@@ -2,13 +2,14 @@ package tools
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"strings"
 )
 
 //读取所有文件读连接池
-func ReadFileAllPool(name string) ([]map[string]interface{}, error) {
+func ReadFileAllPool1(name string) ([]map[string]interface{}, error) {
 	fd, err := os.Open(name)
 	defer fd.Close()
 	if err != nil {
@@ -24,12 +25,50 @@ func ReadFileAllPool(name string) ([]map[string]interface{}, error) {
 		}
 		text := strings.TrimSpace(string(data))
 		if unmarshal, err := JsonUnmarshal([]byte(text)); err != nil {
+			fmt.Printf("err: ----------: %v\n", err)
 			break
 		} else {
 			list = append(list, unmarshal)
 		}
 	}
 
+	return list, nil
+}
+
+func ReadFileAllPool(name string) ([]map[string]interface{}, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		fmt.Println("open file err=", err)
+		return nil, nil
+	}
+
+	//当函数退出时，要及时的关闭file
+	defer file.Close() //要及时关闭file句柄，否则会有内存泄漏.
+
+	// 创建一个 *Reader  ，是带缓冲的
+	/*
+		const (
+		defaultBufSize = 4096 //默认的缓冲区为4096
+		)
+	*/
+	var list []map[string]interface{}
+	reader := bufio.NewReader(file)
+
+	//循环的读取文件的内容
+	for {
+		str, err := reader.ReadString('\n') // 读到一个换行就结束
+		fmt.Printf("bufio.NewReader(file): ----------: %s\n", str)
+		if err == io.EOF { // io.EOF表示文件的末尾
+			break
+		}
+		text := strings.TrimSpace(str)
+		if unmarshal, err := JsonUnmarshal([]byte(text)); err != nil {
+			fmt.Printf("err: ----------: %v\n", err)
+			break
+		} else {
+			list = append(list, unmarshal)
+		}
+	}
 	return list, nil
 }
 
