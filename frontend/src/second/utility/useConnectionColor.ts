c43b27ce6b42d4370acd57111f22bd6c386e2709
tiyongliu <tiyongliu@163.com>
2022-06-08 18:52:10 +0800

    重写iconbar

---
+++ frontend/src/second/utility/useConnectionColor.ts
@@ -0,0 +1,16 @@
+import { useConnectionList } from "../utility/metadataLoaders"
+
+
+export function useConnectionColor(
+  dbid,
+  colorIndex,
+  themeType = null,
+  backgroundStyle = false,
+  useConnectionFallback = true
+){
+  const connections = useConnectionList();
+
+  return{
+
+  }
+}
