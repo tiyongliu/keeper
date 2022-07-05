2cbd338b4e798b30ce65c5c57670993aa2c567e0
tiyongliu <tiyongliu@163.com>
2022-07-05 22:15:06 +0800

    列表显示

--- app/bridge/serverConnections.go
+++ app/bridge/serverConnections.go
@@ -19,9 +19,11 @@ var lock sync.RWMutex
 const conidkey = "conid"
 
 type ServerConnections struct {
-	Closed     map[string]interface{}
-	Opened     []map[string]interface{}
-	LastPinged map[string]code.UnixTime
+	Closed         map[string]interface{}
+	Opened         []map[string]interface{}
+	LastPinged     map[string]code.UnixTime
+	ch             chan *modules.EchoMessage
+	DriverHandlers *sideQuests.MessageDriverHandlers
 }
 
 type OpenedStatus struct {
@@ -29,9 +31,12 @@ type OpenedStatus struct {
 }
 
 func NewServerConnections() *ServerConnections {
+	ch := make(chan *modules.EchoMessage)
 	return &ServerConnections{
-		Closed:     make(map[string]interface{}),
-		LastPinged: make(map[string]code.UnixTime),
+		Closed:         make(map[string]interface{}),
+		LastPinged:     make(map[string]code.UnixTime),
+		ch:             ch,
+		DriverHandlers: sideQuests.NewMessageDriverHandlers(ch),
 	}
 }
 
@@ -89,6 +94,8 @@ func (sc *ServerConnections) ensureOpened(conid string) map[string]interface{} {
 		}
 	})
 
+	runtime.EventsEmit(Application.ctx, "server-status-changed")
+
 	if existing != nil && ok {
 		return existing
 	}
@@ -109,12 +116,8 @@ func (sc *ServerConnections) ensureOpened(conid string) map[string]interface{} {
 		delete(sc.Closed, conid)
 	}
 
-	runtime.EventsEmit(Application.ctx, "server-status-changed")
-
-	ch := make(chan *modules.EchoMessage)
-
-	go sideQuests.NewMessageDriverHandlers(ch).Connect(connection)
-	go sc.listener(conid, ch)
+	go sc.DriverHandlers.Connect(connection)
+	go sc.consumer(newOpened, sc.ch)
 
 	return newOpened
 }
@@ -142,15 +145,15 @@ func (sc *ServerConnections) getCore(conid string, mask bool) map[string]interfa
 
 func (sc *ServerConnections) ServerStatus() interface{} {
 	values := map[string]interface{}{}
-
-	logger.Infof("data11111111111111111111111111111111111111111: %s", tools.ToJsonStr(sc.Opened))
-	logger.Infof("Closed222222222222222222222222222222222222222: %s", tools.ToJsonStr(sc.Closed))
+	logger.Infof("data1111111111111111111111: %s", tools.ToJsonStr(sc.Opened))
+	logger.Infof("Closed2222222: %s", tools.ToJsonStr(sc.Closed))
 
 	for _, driver := range sc.Opened {
 		statusObj, ok := driver["status"].(*OpenedStatus)
 		if ok {
 			values[driver[conidkey].(string)] = statusObj
 		}
+
 	}
 
 	return serializer.SuccessData("", values)
@@ -213,18 +216,25 @@ func (sc *ServerConnections) Refresh(req *RefreshRequest) interface{} {
 	})
 }
 
-func (sc *ServerConnections) listener(conid string, chData <-chan *modules.EchoMessage) {
+//func (sc *ServerConnections) consumer(conid string, chData <-chan *modules.EchoMessage) {
+func (sc *ServerConnections) consumer(newOpened map[string]interface{}, chData <-chan *modules.EchoMessage) {
 	for {
 		message, ok := <-chData
 
 		logger.Infof("chan message -<: %s", tools.ToJsonStr(message))
-
+		conid := newOpened[conidkey].(string)
 		if message != nil {
 			switch message.MsgType {
 			case "status":
 				sc.handleStatus(conid, message.Payload.(*sideQuests.StatusMessage))
 			case "databases":
 				sc.handleDatabases(conid, message.Payload)
+			case "ping":
+				sc.handlePing()
+			case "exit":
+				if !newOpened["disconnected"].(bool) {
+					sc.Close(conid, true)
+				}
 			}
 		}
 
