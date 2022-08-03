1fde2d7b90023bcf8c162cd6e020461467c5e043
liutiyong <422720735@qq.com>
2022-08-04 00:24:53 +0800

    pool conn

---
+++ app/tools/dataDirCore.go
@@ -0,0 +1,28 @@
+package tools
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const MkDir = ".keeper"
+
+func ensureDirectory(dir string, clean bool) {
+	if !IsExist(dir) {
+		if err := os.MkdirAll(filepath.Dir(dir), os.ModePerm); err != nil {
+			log.Fatalf("os.MkdirAll failed err: %v\n", err)
+		}
+	}
+}
+func DataDirCore() string {
+	dir, _ := os.UserHomeDir()
+	return strings.Join([]string{dir, MkDir}, "/")
+}
+
+func DataDir() string {
+	dir := DataDirCore()
+	ensureDirectory(dir, false)
+	return dir
+}
