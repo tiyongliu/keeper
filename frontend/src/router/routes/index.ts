43f17cc43497a0e8a7a0abd2d1b0580649e7050f
tiyongliu <tiyongliu@163.com>
2022-08-07 20:08:53 +0800

    del  demo

--- frontend/src/router/routes/index.ts
+++ frontend/src/router/routes/index.ts
@@ -1,23 +1,10 @@
-import type { AppRouteRecordRaw, AppRouteModule } from '/@/router/types';
+import type { AppRouteRecordRaw } from '/@/router/types';
 
-import { PAGE_NOT_FOUND_ROUTE, REDIRECT_ROUTE } from '/@/router/routes/basic';
-
-import { mainOutRoutes } from './mainOut';
+import { PAGE_NOT_FOUND_ROUTE } from '/@/router/routes/basic';
 import { PageEnum } from '/@/enums/pageEnum';
-import { t } from '/@/hooks/web/useI18n';
 
-// import.meta.globEager() 直接引入所有的模块 Vite 独有的功能
-const modules = import.meta.globEager('./modules/**/*.ts');
-const routeModuleList: AppRouteModule[] = [];
 
-// 加入到路由集合中
-Object.keys(modules).forEach((key) => {
-  const mod = modules[key].default || {};
-  const modList = Array.isArray(mod) ? [...mod] : [mod];
-  routeModuleList.push(...modList);
-});
-
-export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE, ...routeModuleList];
+export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE];
 
 // 根路由
 export const RootRoute: AppRouteRecordRaw = {
@@ -29,21 +16,9 @@ export const RootRoute: AppRouteRecordRaw = {
   },
 };
 
-export const LoginRoute: AppRouteRecordRaw = {
-  path: '/login',
-  name: 'Login',
-  component: () => import('/@/views/sys/login/Login.vue'),
-  meta: {
-    title: t('routes.basic.login'),
-  },
-};
-
 // Basic routing without permission
 // 未经许可的基本路由
 export const basicRoutes = [
-  // LoginRoute,
   RootRoute,
-  ...mainOutRoutes,
-  REDIRECT_ROUTE,
   PAGE_NOT_FOUND_ROUTE,
 ];
