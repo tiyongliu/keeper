a70eef46a9a8f39195c29de3c22c085dc1d09b0a
tiyongliu <tiyongliu@163.com>
2022-08-07 18:33:41 +0800

    create driver

--- app/utility/connectUtility.go
+++ app/utility/connectUtility.go
@@ -10,17 +10,17 @@ import (
 	"sync"
 )
 
-var DriverPoolMap sync.Map
+var storedConnection sync.Map
 
 func CreateEngineDriver(connection map[string]interface{}) (driver standard.SqlStandard, err error) {
 	switch connection["engine"].(string) {
 	case standard.MYSQLALIAS:
-		driver, err = NewMysqlDriver(connection)
+		driver, err = newMysqlDriver(connection)
 		if err != nil {
 			return nil, err
 		}
 	case standard.MONGOALIAS:
-		driver, err = NewMongoDriver(connection)
+		driver, err = newMongoDriver(connection)
 		if err != nil {
 			return nil, err
 		}
@@ -29,10 +29,10 @@ func CreateEngineDriver(connection map[string]interface{}) (driver standard.SqlS
 	return driver, nil
 }
 
-func NewMysqlDriver(connection map[string]interface{}) (standard.SqlStandard, error) {
-	storedConnection := connectUtility(connection)
+func newMysqlDriver(connection map[string]interface{}) (standard.SqlStandard, error) {
+	loadedWithDb := connectUtility(connection)
 	simpleSettingMysql := &modules.SimpleSettingMysql{}
-	err := mapstructure.Decode(storedConnection, simpleSettingMysql)
+	err := mapstructure.Decode(loadedWithDb, simpleSettingMysql)
 	if err != nil {
 		return nil, err
 	}
@@ -45,11 +45,11 @@ func NewMysqlDriver(connection map[string]interface{}) (standard.SqlStandard, er
 	return pool, nil
 }
 
-func NewMongoDriver(connection map[string]interface{}) (standard.SqlStandard, error) {
-	storedConnection := connectUtility(connection)
+func newMongoDriver(connection map[string]interface{}) (standard.SqlStandard, error) {
+	loadedWithDb := connectUtility(connection)
 	pool, err := pluginMongdb.NewSimpleMongoDBPool(&modules.SimpleSettingMongoDB{
-		Host: storedConnection["host"],
-		Port: storedConnection["port"],
+		Host: loadedWithDb["host"],
+		Port: loadedWithDb["port"],
 	})
 
 	if err != nil {
@@ -67,12 +67,12 @@ func SetDriverPool(conid string, driver standard.SqlStandard) error {
 	if driver == nil {
 		return errors.New("invalid memory address or nil pointer dereference")
 	}
-	DriverPoolMap.Store(conid, driver)
+	storedConnection.Store(conid, driver)
 	return nil
 }
 
 func GetDriverPool(conid string) (driver standard.SqlStandard, err error) {
-	load, ok := DriverPoolMap.Load(conid)
+	load, ok := storedConnection.Load(conid)
 	if !ok {
 		return nil, errors.New("invalid memory address or nil pointer dereference")
 	}
@@ -85,7 +85,7 @@ func GetDriverPool(conid string) (driver standard.SqlStandard, err error) {
 }
 
 func DeleteDriverPool(conid string) error {
-	andDelete, ok := DriverPoolMap.LoadAndDelete(conid)
+	andDelete, ok := storedConnection.LoadAndDelete(conid)
 	if ok {
 		if andDelete != nil {
 			return andDelete.(standard.SqlStandard).Close()
--- frontend/src/wailsjs/go/models.ts
+++ frontend/src/wailsjs/go/models.ts
@@ -1,3 +1,28 @@
+export namespace serializer {
+	
+	export class Response {
+	    status: number;
+	    result: any;
+	    message: string;
+	    type: string;
+	    time: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new Response(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.status = source["status"];
+	        this.result = source["result"];
+	        this.message = source["message"];
+	        this.type = source["type"];
+	        this.time = source["time"];
+	    }
+	}
+
+}
+
 export namespace bridge {
 	
 	export class DatabaseRequest {
@@ -42,31 +67,6 @@ export namespace bridge {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.conid = source["conid"];
 	        this.keepOpen = source["keepOpen"];
-	    }
-	}
-
-}
-
-export namespace serializer {
-	
-	export class Response {
-	    status: number;
-	    result: any;
-	    message: string;
-	    type: string;
-	    time: number;
-	
-	    static createFrom(source: any = {}) {
-	        return new Response(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.status = source["status"];
-	        this.result = source["result"];
-	        this.message = source["message"];
-	        this.type = source["type"];
-	        this.time = source["time"];
 	    }
 	}
 
