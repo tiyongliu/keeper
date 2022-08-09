ad1e83c79c2551fd049f5bab1027712eac8c489f
tiyongliu <tiyongliu@163.com>
2022-08-10 00:29:18 +0800

    del

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,9 +7,9 @@ import 'virtual:svg-icons-register';
 import App from './App.vue';
 import { createApp } from 'vue';
 import { initAppConfigStore } from '/@/logics/initAppConfig';
-import { setupErrorHandle } from '/@/logics/error-handle';
-import { router, setupRouter } from '/@/router';
-import { setupRouterGuard } from '/@/router/guard';
+import { setupRouter } from '/@/router';
+// import { router, setupRouter } from '/@/router';
+// import { setupRouterGuard } from '/@/router/guard';
 import { setupStore } from '/@/store';
 import { setupGlobDirectives } from '/@/directives';
 import { setupI18n } from '/@/locales/setupI18n';
@@ -42,15 +42,11 @@ async function bootstrap() {
 
   // router-guard
   // 路由守卫
-  setupRouterGuard(router);
+  // setupRouterGuard(router);
 
   // Register global directive
   // 注册全局指令
   setupGlobDirectives(app);
-
-  // Configure global error handling
-  // 配置全局错误处理
-  setupErrorHandle(app);
 
   // https://next.router.vuejs.org/api/#isready
   // await router.isReady();
