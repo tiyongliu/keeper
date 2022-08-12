3cc649ad758cdcd8a2f5f118005613a3d55b3294
tiyongliu <tiyongliu@163.com>
2022-08-12 21:23:01 +0800

    正在编写查看列表

--- app/pkg/containers/sideQuestCh.go
+++ app/pkg/containers/sideQuestCh.go
@@ -2,6 +2,7 @@ package containers
 
 import (
 	"keeper/app/pkg/standard"
+	"keeper/app/utility"
 )
 
 type EchoMessage struct {
@@ -16,11 +17,22 @@ type OpenedStatus struct {
 	Message string `json:"message"`
 }
 
-type OpenedData struct {
+type OpenedServerConnection struct {
 	Conid        string                 `json:"conid"`
 	Status       *OpenedStatus          `json:"status"`
 	Databases    interface{}            `json:"databases"`
 	Connection   map[string]interface{} `json:"connection"`
 	Disconnected bool                   `json:"disconnected"`
 	Version      *standard.VersionMsg   `json:"version"`
+}
+
+type OpenedDatabaseConnection struct {
+	Conid         string                 `json:"conid"`
+	Status        *OpenedStatus          `json:"status"`
+	Database      string                 `json:"database"`
+	Connection    map[string]interface{} `json:"connection"`
+	ServerVersion *standard.VersionMsg   `json:"version"`
+	Structure     interface{}            `json:"structure"`
+	AnalysedTime  utility.UnixTime       `json:"analysedTime"`
+	Disconnected  bool                   `json:"disconnected"`
 }
