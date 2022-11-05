ea2f42aa955afc784bffb4ff4de868846b637d40
liutiyong <tiyongliu@163.com>
2022-11-05 23:32:40 +0800

    edit DataFilterControl

--- frontend/src/second/datagrid/DataFilterControl.vue
+++ frontend/src/second/datagrid/DataFilterControl.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="flex"></div>
+  <div class="flex">
+    <InlineButton v-if="customCommandIcon" :title="customCommandTooltip">
+      <FontIcon :icon="customCommandIcon"/>
+    </InlineButton>
+    <template v-if="conid && database && driver">
+      <InlineButton
+        v-if="driver?.databaseEngineTypes?.includes('sql') && foreignKey"
+        narrow square>
+        <FontIcon icon="icon dots-horizontal" />
+      </InlineButton>
+      <InlineButton
+        v-else-if="(pureName && columnName) ||
+        (pureName && uniqueName && driver?.databaseEngineTypes?.includes('document'))"
+        narrow square>
+        <FontIcon icon="icon dots-vertical" />
+      </InlineButton>
+    </template>
+    <template v-else-if="jslid">
+      <InlineButton narrow square>
+        <FontIcon icon="icon dots-vertical" />
+      </InlineButton>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, PropType} from 'vue'
+import {defineComponent, PropType, toRefs} from 'vue'
 import InlineButton from '/@/second/buttons/InlineButton.vue'
 import FontIcon from '/@/second/icons/FontIcon.vue'
+import {EngineDriver} from '/@/second/keeper-types'
 export default defineComponent({
   name: 'DataFilterControl',
   components: {
@@ -17,11 +40,20 @@ export default defineComponent({
       type: Boolean as PropType<boolean>,
       default: false
     },
+    foreignKey: {
+      type: Object as PropType<{ refTableName: string }>
+    },
     conid: {
       type: String as PropType<string>
     },
     database: {
       type: String as PropType<string>
+    },
+    driver: {
+      type: Object as PropType<EngineDriver>
+    },
+    jslid: {
+      type: [String, Number] as PropType<string | number>
     },
     pureName: {
       type: String as PropType<string>
@@ -35,9 +67,17 @@ export default defineComponent({
     uniqueName: {
       type: String as PropType<string>
     },
+    customCommandIcon: {
+      type: String as PropType<string>
+    },
+    customCommandTooltip: {
+      type: String as PropType<string>
+    }
   },
-  setup() {
-
+  setup(props) {
+    return {
+      ...toRefs(props),
+    }
   }
 })
 </script>
