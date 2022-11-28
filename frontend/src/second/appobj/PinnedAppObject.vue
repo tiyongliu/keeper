ed229e447a9bd12c3202bd261070bee512c808f9
tiyongliu <tiyongliu@163.com>
2022-11-28 13:31:35 +0800

    fix PinnedAppObject.vue click pin、unpin database tables

---
+++ frontend/src/second/appobj/PinnedAppObject.vue
@@ -0,0 +1,34 @@
+<template>
+  <template v-if="data">
+    <DatabaseObjectAppObject
+      v-if="data && data?.objectTypeField"
+      v-bind="Object.assign({}, $props, $attrs)"/>
+    <DatabaseAppObject
+      v-else
+      v-bind="Object.assign({}, $props, $attrs)"/>
+  </template>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType, toRefs} from 'vue'
+import DatabaseAppObject from './DatabaseAppObject'
+import DatabaseObjectAppObject from './DatabaseObjectAppObject'
+import {IPinnedDatabasesItem} from "/@/second/typings/types/standard.d";
+export default defineComponent({
+  name: 'PinnedAppObject',
+  props: {
+    data: {
+      type: Object as PropType<IPinnedDatabasesItem>,
+    },
+  },
+  components: {
+    DatabaseAppObject,
+    DatabaseObjectAppObject
+  },
+  setup(props) {
+    return {
+      ...toRefs(props)
+    }
+  }
+})
+</script>
